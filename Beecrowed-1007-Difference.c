d958cf715d97e8b65185665a9d76baad041573b3
Rajib Hasan <RajibHasan.MTE@gmail.com>
2025-05-03 23:40:36 +0600

    solve problem 1007

---
+++ Beecrowed-1007-Difference.c
@@ -0,0 +1,18 @@
+
+/************************************
+Problem: Beecrowed-1005 - Average 1
+Link: https://judge.beecrowd.com/en/problems/view/1007
+Beecrowed profile: https://judge.beecrowd.com/en/profile/632822
+Author: Rajib Hasan
+Date: 15/11/22, 1:00 AM
+************************************/
+
+#include<stdio.h>
+
+int main(){
+    int a, b, c, d, diferencea;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    diferencea = ((a*b)-(c*d));
+    printf("DIFERENCA = %d\n", diferencea);
+    return 0;
+}
