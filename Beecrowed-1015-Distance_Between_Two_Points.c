bc34d991a2843c1e6c65f86189d46480c95aaa46
Rajib Hasan <RajibHasan.MTE@gmail.com>
2025-05-05 14:10:23 +0600

    added 1015 and 1016

---
+++ Beecrowed-1015-Distance_Between_Two_Points.c
@@ -0,0 +1,25 @@
+/************************************
+Problem: Beecrowed-1014 Distance and two points
+Link: https://judge.beecrowd.com/en/problems/view/115
+Beecrowed profile: https://judge.beecrowd.com/en/profile/632822
+Author: Rajib Hasan
+Date: 20/11/22, 1:00 AM
+************************************/
+
+
+#include<stdio.h>
+#include<math.h>
+
+
+int main(){
+    double x1, y1, x2, y2;
+    scanf("%lf %lf", &x1, &y1);
+    scanf("%lf %lf", &x2, &y2);
+
+    double distance = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+
+    printf("%.4lf\n", distance);
+    return 0;
+
+
+}
---
+++ Beecrowed-1016-Distance.c
@@ -0,0 +1,20 @@
+/************************************
+Problem: Beecrowed-1014 distance
+Link: https://judge.beecrowd.com/en/problems/view/116
+Beecrowed profile: https://judge.beecrowd.com/en/profile/632822
+Author: Rajib Hasan
+Date: 5/11/22, 2:00 AM
+************************************/
+
+
+#include<stdio.h>
+
+int main(){
+    int a;
+    scanf("%d", &a);
+
+    printf("%d minutos\n", a*2);
+    return 0;
+
+
+}
