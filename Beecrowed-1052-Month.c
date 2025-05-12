0a932f38abcc0ecd5a370ef13292be30df96b768
Rajib Hasan <RajibHasan.MTE@gmail.com>
2025-05-12 16:43:15 +0600

    added 1052

---
+++ Beecrowed-1052-Month.c
@@ -0,0 +1,32 @@
+/************************************
+Problem: Beecrowed-1052
+Link: https://judge.beecrowd.com/en/problems/view/1052
+Beecrowed profile: https://judge.beecrowd.com/en/profile/632822
+Author: Rajib Hasan
+
+************************************/
+
+
+#include<stdio.h>
+
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    if(n == 1) printf("January\n");
+    else if(n ==2) printf("February\n");
+    else if(n == 3) printf("March\n");
+    else if(n == 4) printf("Aprill\n");
+    else if(n == 5) printf("May\n");
+    else if(n == 6) printf("June\n");
+    else if(n == 7) printf("July\n");
+    else if(n == 8) printf("August\n");
+    else if(n == 9) printf("September\n");
+    else if(n == 10) printf("October\n");
+    else if(n == 11) printf("November\n");
+    else if(n == 12) printf("December\n");
+
+
+return 0;
+}
