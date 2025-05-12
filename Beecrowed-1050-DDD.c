c2539a6ca01c8698b84fdb7e167693b060a3eca9
Rajib Hasan <RajibHasan.MTE@gmail.com>
2025-05-12 16:39:24 +0600

    added 1050

---
+++ Beecrowed-1050-DDD.c
@@ -0,0 +1,30 @@
+/************************************
+Problem: Beecrowed-1050
+Link: https://judge.beecrowd.com/en/problems/view/1050
+Beecrowed profile: https://judge.beecrowd.com/en/profile/632822
+Author: Rajib Hasan
+
+************************************/
+
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    if(n == 61) printf("Brasilia\n");
+    else if(n == 71) printf("Salvador\n");
+    else if(n == 11) printf("Sao Paulo\n");
+    else if(n == 21) printf("Rio de Janeiro\n");
+    else if(n == 32) printf("Juiz de Fora\n");
+    else if(n == 19) printf("Campinas\n");
+    else if(n == 27) printf("vitoria\n");
+    else if(n == 31) printf("Belo Horizonte\n");
+    else printf("%d nao cadastrado\n", n);
+
+    return 0;
+
+
+
+}
