c9ac1fcf245fe369ce1be24b9b2e91e1848dee91
Rajib Hasan <RajibHasan.MTE@gmail.com>
2025-05-12 16:36:41 +0600

    not complete 1049 problem

---
+++ Beecrowed-1049-Animal.c
@@ -0,0 +1,8 @@
+/************************************
+Problem: Beecrowed-1049 Animal
+Link: https://judge.beecrowd.com/en/problems/view/1049
+Beecrowed profile: https://judge.beecrowd.com/en/profile/632822
+Author: Rajib Hasan
+
+************************************/
+
