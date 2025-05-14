6ae756bee3ea137301f860628256c8c47899a9b4
Rajib Hasan <RajibHasan.MTE@gmail.com>
2025-05-14 12:11:14 +0600

    comment add to the 1237 problem

--- 3. STRINGS/Beecrowd-1237-Compare_Substring.c
+++ 3. STRINGS/Beecrowd-1237-Compare_Substring.c
@@ -1,17 +1,17 @@
-
 #include <stdio.h>
 #include <string.h>
 
+// Function to find the length of the longest common substring
 int longestCommonSubstring(char *str1, char *str2, int sizeOfstr1, int sizeOfstr2);
 
 int main()
 {
     char str1[51] = {""}, str2[51] = {""};
 
+    // Loop to read input until EOF (End Of File)
     while (scanf(" %[^\n] %[^\n]", str1, str2) != EOF)
     {
-
-
+        // Call the function and print the result
         printf("%d\n", longestCommonSubstring(str1, str2, strlen(str1), strlen(str2)));
     }
 
@@ -20,27 +20,39 @@ int main()
 
 int longestCommonSubstring(char *str1, char *str2, int sizeOfstr1, int sizeOfstr2)
 {
-    int i = 0, j = 0, x = 0, y = 0, sizeOfSubstring = 0, maxSizeOfSubstring = 0;
+    int i, j, x, y;
+    int sizeOfSubstring = 0;      // Current length of matching substring
+    int maxSizeOfSubstring = 0;   // Maximum found matching substring length
 
-    for (; i < sizeOfstr1; i++)
+    // Loop over each character of the first string
+    for (i = 0; i < sizeOfstr1; i++)
     {
+        // Loop over each character of the second string
         for (j = 0; j < sizeOfstr2; j++)
         {
-            for (x = i, y = j; y < sizeOfstr2; x++, y++)
+            sizeOfSubstring = 0;  // Reset the current substring length
+
+            // Start matching characters from str1[i..] and str2[j..]
+            for (x = i, y = j; x < sizeOfstr1 && y < sizeOfstr2; x++, y++)
             {
+                // If characters match, increase the length
                 if (str1[x] == str2[y])
                 {
                     sizeOfSubstring++;
+
+                    // Update the maximum if current match is longer
+                    if (sizeOfSubstring > maxSizeOfSubstring)
+                        maxSizeOfSubstring = sizeOfSubstring;
                 }
                 else
                 {
-                    maxSizeOfSubstring = sizeOfSubstring > maxSizeOfSubstring ? sizeOfSubstring : maxSizeOfSubstring;
-                    sizeOfSubstring = 0;
+                    // If characters do not match, stop the inner matching loop
                     break;
                 }
             }
         }
     }
 
+    // Return the maximum length of common substring found
     return maxSizeOfSubstring;
 }
